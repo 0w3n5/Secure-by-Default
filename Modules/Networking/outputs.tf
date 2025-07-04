f529cd3ce7e6faca22450e0cfd0f856f357e7e75
Owen Sweet <67649781+0w3n5@users.noreply.github.com>
2025-07-04 17:32:40 +0100

    Create outputs.tf

---
+++ Modules/Networking/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value       = aws_vpc.main.id
+  description = "The ID of the VPC"
+}
+
+output "public_subnet_id" {
+  value       = aws_subnet.public.id
+  description = "Public subnet ID"
+}
+
+output "private_subnet_id" {
+  value       = aws_subnet.private.id
+  description = "Private subnet ID"
+}
+
+output "nat_gateway_id" {
+  value       = aws_nat_gateway.nat.id
+  description = "NAT Gateway ID"
+}
