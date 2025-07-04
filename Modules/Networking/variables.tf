6eb53170e80f06b24029f9e5c3e79aece54f9c28
Owen Sweet <67649781+0w3n5@users.noreply.github.com>
2025-07-04 17:30:54 +0100

    Create variables.tf

---
+++ Modules/Networking/variables.tf
@@ -0,0 +1,25 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
