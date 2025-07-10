f932d1dd01d552f15fb7ea65846555a9236930c9
Owen Sweet <67649781+0w3n5@users.noreply.github.com>
2025-07-10 08:34:24 +0100

    Update output.tf

--- Modules/guardduty/output.tf
+++ Modules/guardduty/output.tf
@@ -2,3 +2,13 @@ output "guardduty_detector_id" {
   description = "GuardDuty Detector ID"
   value       = aws_guardduty_detector.this.id
 }
+
+output "guardduty_event_rule_name" {
+  description = "EventBridge rule capturing GuardDuty findings"
+  value       = aws_cloudwatch_event_rule.gd_finding_rule.name
+}
+
+output "cloudwatch_log_group_name" {
+  description = "CloudWatch Log Group where findings are published"
+  value       = aws_cloudwatch_log_group.gd_log_group.name
+}
